632969480b0f116d16e651eadd25efa9982fd914
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-07-14 01:42:22 +0900

    Add: 快速の判別を実装 (#8)

--- time_table_scraper/main.py
+++ time_table_scraper/main.py
@@ -1,21 +1,37 @@
-from typing import Final
+from typing import Final, Any, List, Tuple
 from datetime import datetime
 import requests
 
 from bs4 import BeautifulSoup
 
-from my_types import NavitimeParametersType, TrainType, ResponseType
+from my_types import NavitimeParametersType, ResponseType
 
 BASE_URL: Final[str] = 'https://www.navitime.co.jp/transfer/searchlist'
 
 
-def format_time_str(time_str: str) -> TrainType:
-    departure_time, arrival_time = time_str.split(u'\xa0⇒\xa0', 2)
-    result: TrainType = {
-        'time': (departure_time, arrival_time),
-        'type': 'normal'
+def get_time_table(rows: List[Any], sections: List[Any]) -> ResponseType:
+    return {
+        'time_table': [
+            {
+                'time': get_train_time(rows[i]),
+                'type': get_train_type(sections[i])
+            } for i in range(len(rows))
+        ]
     }
-    return result
+
+
+def get_train_time(row: Any) -> Tuple[str, str]:
+    time_str = row.find('dt', class_='left').text
+    departure_time, arrival_time = time_str.split(u'\xa0⇒\xa0', 1)
+    return (departure_time, arrival_time)
+
+
+def get_train_type(section: Any) -> str:
+    section_str = section.text
+    if '快速' in section_str:
+        return 'rapid'
+    else:
+        return 'normal'
 
 
 def time_table_scraper(starting_point: str, end_point: str) -> ResponseType:
@@ -48,11 +64,8 @@ def time_table_scraper(starting_point: str, end_point: str) -> ResponseType:
 
     # 時刻表の取得
     rows = page.find_all('dl', class_='summay_route')
+    sections = page.find_all('div', class_='section_detail_frame')
     # 時刻の取得
-    time_table = [format_time_str(row.find('dt', class_='left').text) for row in rows]
-
-    result: ResponseType = {
-        'time_table': time_table
-    }
+    result: ResponseType = get_time_table(rows, sections)
 
     return result
