82fbe00bf0517dddab52b9ef19b32470ee5f541e
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-07-15 16:02:33 +0900

    Remove: mockを削除

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ def read_root():
     return {'Hello': 'World'}
 
 
-@app.get('/mock')
-def get_mock_data(starting_point: str, end_point: str) -> ResponseType:
-    response: ResponseType = {
-        'time_table': [
-            {'time': ('07:18', '08:24'), 'type': 'local'},
-            {'time': ('07:35', '08:33'), 'type': 'rapid'},
-            {'time': ('08:06', '09:02'), 'type': 'local'}
-        ]
-    }
-    return response
-
-
 @app.get('/scraping')
 def get_time_table(starting_point: str, end_point: str) -> ResponseType:
     response: ResponseType = time_table_scraper(starting_point, end_point)
